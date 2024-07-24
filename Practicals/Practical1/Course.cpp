79f427aa21deabdaddf284bc828f6e19cb2bd83f
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-07-24 15:05:20 +0200

    Implemented some functions

--- Practicals/Practical1/Course.cpp
+++ Practicals/Practical1/Course.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "Course.h"
+
+Course::Course(std::string description, int maxNumberOfItems) : description(description), maxNumberOfItems(maxNumberOfItems){}
+
+bool Course::addMenuItem(std::string description, float price, int stock) {
+
+    bool found = false;
+
+    for(const auto& item : menuItems) {
+        if(item->getDescription() == description) {
+            found = true;
+            break;
+        }
+    }
+
+    if(!found) {
+        menuItems.push_back(new MenuItem(description,price,stock));
+        return true;
+    }
+
+    return false;
+}
+
+void Course::printMenuItems() {
+    char index = 'a';
+
+    for(const auto& item : menuItems) {
+        std::cout << '\t' << index << "." << '\t' << item->getDescription() << std::endl;
+        index++;
+    }
+}
+
--- Practicals/Practical1/Course.h
+++ Practicals/Practical1/Course.h
@@ -5,10 +5,28 @@
 #ifndef COURSE_H
 #define COURSE_H
 
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "MenuItem.h"
 
 
 class Course {
+protected:
+    std::string description;
+    int maxNumberOfItems;
+    std::vector<MenuItem*> menuItems;
 
+public:
+    Course(std::string description, int maxNumberOfItems);
+    std::string getDescription();
+    bool addMenuItem(std::string description, float price, int stock);
+    void printMenuItems();
+    void printInventory();
+    MenuItem* getMenuItem(int index);
+    virtual void recommendBeverage();
+    ~Course();
 };
 
 
