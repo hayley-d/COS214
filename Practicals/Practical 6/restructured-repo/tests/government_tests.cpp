d4ce04199ec76f4a3fbe397380c2a3aec3d54134
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-11-03 15:57:09 +0200

    v1 of game

--- Practicals/Practical 6/restructured-repo/tests/government_tests.cpp
+++ Practicals/Practical 6/restructured-repo/tests/government_tests.cpp
@@ -3,81 +3,111 @@
 #include "../headers/CitizenType.h"
 #include "../headers/TaxAuthority.h"
 #include "../headers/Building.h"
-#include "../headers/Factory.h"
-#include "../headers/BrickFactory.h"
-#include "../headers/FactoryFactory.h"
+#include "../headers/City.h"
+#include "../headers/Government.h"
 #include "../headers/BuildingType.h"
-#include "../headers/Residential.h"
-#include "../headers/ResidentialFactory.h"
+#include "../headers/VehicleType.h"
+#include "../headers/Command.h"
+#include "../headers/BuildCommand.h"
+#include "../headers/ChangeTaxStrategyCommand.h"
+#include "../headers/CityReportCommand.h"
+#include "../headers/IncreaseTransportCommand.h"
+#include "../headers/RepairUtilitiesCommand.h"
+#include "../headers/UpgradeUtilitiesCommand.h"
+
+
+#include <iostream>
+#include <string>
 
 #include <memory>
 
 class government_test : public ::testing::Test {
     protected:
-        std::shared_ptr<TaxAuthority> taxAuth;
-        
+        std::shared_ptr<City> city;
+        BuildCommand* buildC;
+        ChangeTaxStrategyCommand* strategyC;
+        CityReportCommand* reportC;
+        IncreaseTransportCommand* incTrC;
+        RepairUtilitiesCommand* repairC;
+        UpgradeUtilitiesCommand* upC;
+        std::string city_report;
+
         void SetUp() override {
-            taxAuth = std::make_shared<TaxAuthority>();
+            this->city = std::make_shared<City>();
+            buildC = new BuildCommand(city,BuildingType::BrickFactory);
+            strategyC = new ChangeTaxStrategyCommand(city,TaxType::Flat);
+            reportC = new CityReportCommand(city, city_report);
+            incTrC = new IncreaseTransportCommand(city,VehicleType::Taxi);
+            repairC = new RepairUtilitiesCommand(city);
+            upC = new UpgradeUtilitiesCommand(city);
         }
         
         void TearDown() override {
+            delete buildC;
+            delete strategyC;
+            delete reportC;
+            delete incTrC;
+            delete repairC;
+            delete upC;
         }
 };
 
 // Test Constructor
-/*TEST_F(government_test, construction_test) {
-    EXPECT_EQ(c->getId(),0);
-    EXPECT_EQ(c->getType(),CitizenType::Citizen);
-    EXPECT_EQ(c->getSatisfactionLevel(),100);
-    EXPECT_EQ(c->getFunds(),300);
-    EXPECT_EQ(c->hasHome(),false);
-    EXPECT_EQ(c->getEmploymentStatus(),false);
-    EXPECT_EQ(c->isInVehicle(),false);
+TEST_F(government_test, construction_test) {
+    EXPECT_EQ(city->getState(),CityState::Happy);
+    EXPECT_EQ(city->checkCityFunds(),20000);
+    EXPECT_EQ(city->getCitizenCount(),0);
+    EXPECT_EQ(city->getWorkerCount(),0);
+    EXPECT_EQ(city->getBuildingCount(),0);
+
+    city->startTaxCycle();
+    EXPECT_EQ(city->checkCityFunds(),20000);
+
+    EXPECT_EQ(city->getCitizenHappiness(),0);
+    city->setCitizenHappiness(100);
+    EXPECT_EQ(city->getCitizenHappiness(),100);
+    city->calculateHappiness();
+    EXPECT_EQ(city->getCitizenHappiness(),0);
+    EXPECT_EQ(city->getState(),CityState::Upset);
+
+    city->removeLastBuilding();
 
 }
 
-TEST_F(citizen_test, citizen_tax_test) {
-    BrickFactory* workPtr = dynamic_cast<BrickFactory*>(work);
-
-    c->payTaxes(100);
-    EXPECT_EQ(c->getFunds(),300);
-    workPtr->employ(*c);
-    EXPECT_EQ(c->getEmploymentStatus(),true);
-    EXPECT_EQ(c->getType(),CitizenType::Worker);
-    c->payTaxes(100);
-    EXPECT_EQ(c->getFunds(),200);
-    c->workDay();
-    EXPECT_EQ(c->getFunds(),300);
-
+TEST_F(government_test, tax_test) {
+    EXPECT_EQ(city->checkCityFunds(),20000);
+    city->startTaxCycle();
+    EXPECT_EQ(city->checkCityFunds(),20000);
+    city->addBuilding(BuildingType::BrickFactory);
+    city->startTaxCycle();
+    EXPECT_GT(city->checkCityFunds(),20000);
+    EXPECT_GT(city->getCitizenCount(),0);
+    EXPECT_GT(city->getWorkerCount(),0);
+    EXPECT_EQ(city->getBuildingCount(),1);
 }
 
-TEST_F(citizen_test, citizen_home_test) {
-    Residential* homePtr = dynamic_cast<Residential*>(home);
-    EXPECT_EQ(c->hasHome(),false);
-    EXPECT_EQ(homePtr->isFull(),false);
-    homePtr->addTenant(*c);
-    EXPECT_EQ(c->hasHome(),true);
-
+TEST_F(government_test, utility_test) {
+    city->checkUtilityUsage();
+    city->upgradeUtilities();
+    city->repairUtilities();
+    std::string s = "";
+    city->getUtilityStats(s);
 }
 
-TEST_F(citizen_test, citizen_fire_test) {
-    BrickFactory* workPtr = dynamic_cast<BrickFactory*>(work);
-    workPtr->employ(*c);
-    EXPECT_EQ(c->getEmploymentStatus(),true);
-    EXPECT_EQ(c->getType(),CitizenType::Worker);
-    workPtr->fire(*c);
-    EXPECT_EQ(c->getEmploymentStatus(),false);
-    EXPECT_EQ(c->getType(),CitizenType::Citizen);
+TEST_F(government_test, transport_test) {
+    std::string temp = "";
+    city->generateReport(temp);
+
+    std::cout << temp << std::endl;
+
+    city->increaseTransport(VehicleType::Train);
+    city->increaseTransport(VehicleType::Taxi);
+
+    buildC->execute();
+    strategyC->execute();
+    reportC->execute();
+    incTrC->execute();
+    repairC->execute();
+    upC->execute();
 
 }
-
-TEST_F(citizen_test, citizen_Retire_test) {
-    BrickFactory* workPtr = dynamic_cast<BrickFactory*>(work);
-    workPtr->employ(*c);
-    EXPECT_EQ(c->getEmploymentStatus(),true);
-    EXPECT_EQ(c->getType(),CitizenType::Worker);
-    workPtr->retire(*c);
-    EXPECT_EQ(c->getEmploymentStatus(),false);
-    EXPECT_EQ(c->getType(),CitizenType::Retired);
-    EXPECT_EQ(c->hasHome(),false);
-}*/
